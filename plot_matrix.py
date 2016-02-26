840cc0cd361fc0c29730b94aeae57895950eaf45
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2016-02-26 15:19:32 +0100

    add litle plotting script to show showers and neighbours and stuff

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,6 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+a = np.loadtxt("neighbours.txt")
+plt.imshow(a, interpolation='nearest', aspect='auto', cmap='inferno')
+plt.savefig("neighbours.pdf")
+
+
 a = np.loadtxt("matrix.txt")
 plt.imshow(a, interpolation='nearest', aspect='auto', cmap='inferno')
 plt.savefig("matrix.pdf")
+
+
+a = np.loadtxt("shower.txt")
+plt.imshow(a, interpolation='nearest', aspect='auto', cmap='inferno')
+plt.savefig("shower.pdf")
